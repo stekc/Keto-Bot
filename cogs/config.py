995b8b3f570866dbb0f9e6d8752985ac78c115db
Nebula <me@itsnebula.net>
2024-10-05 22:45:45 -0400

    lint: run isort

--- cogs/config.py
+++ cogs/config.py
@@ -3,7 +3,7 @@ from typing import List
 
 import aiosqlite
 import discord
-from discord import app_commands, Interaction
+from discord import Interaction, app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
 
